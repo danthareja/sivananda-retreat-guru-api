3a4152e78e181abefa85ea0b0eb4b8271b6ec26a
Dan Thareja <danthareja@gmail.com>
2018-09-26 18:59:35 +0200

    Reuse Next error component

--- pages/_error.js
+++ pages/_error.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
-import HTTPStatus from 'http-status'
-import Head from 'next/head'
+import NextError from 'next/error'
 
 export default class Error extends Component {
   static getInitialProps({ res, err }) {
@@ -26,22 +25,8 @@ export default class Error extends Component {
       )
     }
 
-    // Next.js Error
-    const title = statusCode === 404
-      ? 'This page could not be found'
-      : HTTPStatus[statusCode] || 'An unexpected error has occurred';
-
     return (
-      <div>
-        <Head>
-          <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-          <title>{statusCode}: {title}</title>
-        </Head>
-        <div>
-          <h1>Error</h1>
-          <p>{statusCode}: {title}</p>
-        </div>
-      </div>
+      <NextError statusCode={this.props.statusCode} />
     )
   }
 }
